d51034a1136c1d6963d8474cfc9d57e470a5e116
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-05-13 18:19:58 -0300

    comentario aclaracion

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['cdn.dummyjson.com'], // Corrige el dominio aquí
+  images: {//esto iba en un mjs (creo) pero no tengo ese archivo
+    domains: ['cdn.dummyjson.com'], 
   },
 };
 
