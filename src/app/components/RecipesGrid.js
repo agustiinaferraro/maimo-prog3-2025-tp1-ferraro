6402f22dd7de97cacd113b1ae12b3e05785636b9
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-05-15 21:40:59 -0300

    data state

--- src/app/components/RecipesGrid.js
+++ src/app/components/RecipesGrid.js
@@ -1,5 +1,11 @@
+'use client'
+
+import { useState, useEffect } from "react"; 
+//el usestate es una forma de almacenar informacion que tiene el componennte, y cuando yo la grabo en una funcion, al momento de grabar esa info en una variable, el metodo return de abajo se va a vovler a ejecutar solo, entonces, con el estado que tenga en ese momento se va a volver a ejecutar y se van a ver los cambios que guardamos esa variable. oSEA, SE ACTULIZA SOLO ESE COMPONENTE, NO TODA LA PAGINA.
+//el use effect: cuando el componente se carga ren la pantalla, el useffect se va a ejecutar automaticamente, entonces cuando se carga, el resultado de esa data se guarda en el state, ahi pasa todo. Es una especie de varieble  que al guardarle data se vuelve a ejecutar el metodo return.
 import RecipeCard from "@/src/app/components/RecipeCard";
-import parsedData from "@/src/data/recipes";
+import parsedData from "@/src/data/recipes"; //despues borrar
+import axios from 'axios'
 
 
 /*const RecipesGrid = () => {
@@ -17,9 +23,28 @@ import parsedData from "@/src/data/recipes";
 }*/
 
 const RecipesGrid = () => {
+  //en este espacio, entre el componente y el return, va solo javascript (puedo implementatr html con ``)
+  const [data, setData] = useState([]) //en los corchetes siempre va una variable y un metodo que grabe el state. Si la variable se llama data, el metodo que guarda la data es set+lavariablequeguardaladata
+//en los parentesis se pone el valor inicial que va a tener la variable, es dinde voya  inicializar la variable
+
+useEffect(()=>{
+
+  const getData = async () => {
+
+    const response = await axios.get('https://dummyjson.com/recipes')//en el get entre comillas va el link a las recetas
+    const responseData = response.data.recipes
+    setData(responseData)//lo que le pase va a gauardarlo en la const data
+  }
+
+  getData()
+
+
+}, [])
+
+
   return (
     <div className="recipes_grid">
-      {parsedData.recipes.map((recipe) => {
+      {data.map((recipe) => {
         // return de la card
         return <RecipeCard key={recipe.id} name={recipe.name} image={recipe.image} />;
       })}
