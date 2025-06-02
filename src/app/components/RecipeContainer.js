25716c0c87d811a0e2b3bfb905e8827bc7a4535d
Agustina Ferraro <ferraroagustina25@gmail.com>
2025-06-02 19:24:22 -0300

    listas, numeros, estilos en recipeContainer, instrucciones

--- src/app/components/RecipeContainer.js
+++ src/app/components/RecipeContainer.js
@@ -9,50 +9,55 @@ const RecipeContainer = ({id}) => {
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
 
-
   useEffect (()=> {
-
     const fetchRecipe = async () => {
-    setLoading(true)
-    try {
-    const response = await axios.get(
-      `https://dummyjson.com/recipes/${id}`
-    );//en el get entre comillas va el link a las recetas
-    setRecipe(response.data);  // Guarda la receta completa en recipes
-    setLoading(false)
-    } catch (error) {
-      setError(true)
-      setLoading(false)
-    }
-  };
+      setLoading(true)
+      try {
+        const response = await axios.get(
+          `https://dummyjson.com/recipes/${id}`
+        ); //en el get entre comillas va el link a las recetas
+        setRecipe(response.data);  // Guarda la receta completa en recipes
+        setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+      }
+    };
 
-  fetchRecipe ()
-
+    fetchRecipe()
   }, [id])
 
-
-      if (loading) return <Loading />;
-    if (error) return <div>Hubo un error</div>;
-    if (!recipe) return <div>No se encontró la receta</div>
-
+  if (loading) return <Loading />;
+  if (error) return <div>Hubo un error</div>;
+  if (!recipe) return <div>No se encontró la receta</div>
 
   return (
-  
+    <div className="recipeContainer">
 
-      <div>
-      <h2>{recipe.name}</h2>
-      <img src={recipe.image} alt={recipe.name} width={300} />
-        {
-      
-      <ul>
-      {recipe.ingredients.map((ingredient, index) => (
-        <li key={index}>{ingredient}</li> //index es la posicion del ingrediente
-      ))}
-    </ul>
+      <div className="grilla">
+        <div className="imgContainer">
+          <h2>{recipe.name}</h2>
+          <img className="img" src={recipe.image} alt={recipe.name} width={300} />
+          {/* imagen de la receta */}
+        </div>
 
-      
-      }
+        <div className="ingredients">
+          <ul>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient} {/* index es la posición del ingrediente */} </li> 
+            ))}
+          </ul>
+          {/* lista de ingredientes */}
+        </div>
       </div>
+
+      <ul className="instructions">
+        {recipe.instructions.map((instruction, index) => (
+          <li key={index}>{instruction} {/* index es la posición de la instrccion */}</li> 
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
